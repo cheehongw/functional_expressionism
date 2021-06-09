06659e02dc63d5d9eb887b82186570029521c95d
Chee Hong <c.h.wong2606@gmail.com>
2021-06-09 22:18:27 +0800

    Add title and backgrond image to index view.
    Adds title and background image to index view. Refactored the CSS for App.js
    into a CSS class file.

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Header  from '../../components/Header.js';
+import React from 'react';
+import Header from '../../components/Header.js';
 import ClickableCard from '../../components/ClickableCard.js';
+import { Container, Typography } from '@material-ui/core';
+import styles from './App.module.css';
+
 
 export default function App() {
+
   return (
     <div className="">
-      <Header URL="/" />
+      <Header className={styles.appHeader} URL="/" />
+
+      <div className={styles.title} disableGutters={true}>
+        <Typography className={styles.text} variant='h1'>
+          TinFood
+        </Typography>
+      </div>
+
+
       <ClickableCard URL="/locations">Locations</ClickableCard>
       <ClickableCard URL='/suggestions'>I'm Feeling Lucky</ClickableCard>
     </div>
