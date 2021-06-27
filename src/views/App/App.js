00d01973e396df6078d35bb2823a406d2635e3c8
Chee Hong <c.h.wong2606@gmail.com>
2021-06-28 03:41:20 +0800

    Improve look of index page.
    closes #5
    
    Responsive design may need further improvements.

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -3,6 +3,9 @@ import Header from '../../components/Header.js';
 import ClickableCard from '../../components/ClickableCard.js';
 import styles from './App.module.css';
 import { Typography } from '@material-ui/core';
+import { ReactComponent as StallIcon} from '../../assets/food-stall.svg';
+import { ReactComponent as Clover} from '../../assets/clover.svg';
+
 
 export default function App() {
   return (
@@ -10,20 +13,30 @@ export default function App() {
       <Header className={styles.appHeader} URL="/" />
 
       <div className={styles.title}>
-        <h1 className={styles.text}>TinFood</h1>
+        <img className={styles.img} 
+          src='https://upload.wikimedia.org/wikipedia/commons/0/0d/National_University_of_Singapore%2C_panorama%2C_Nov_06.jpg'
+          alt='TinFood homepage'/>
+        <h1 className={styles.text}>TINFOOD</h1>
       </div>
+
 
       <div className={styles.options}>
         <ClickableCard URL="/locations">
-          <Typography>
+          <div className={styles.cardContent}>
+          <Typography variant="h6">
             Locations
           </Typography>
+          <StallIcon className={styles.icon} />
+          </div>
         </ClickableCard>
         
         <ClickableCard URL='/suggestions'>
-          <Typography>
+        <div className={styles.cardContent}>
+          <Typography variant="h6">
             I'm Feeling Lucky
           </Typography>
+          <Clover className={styles.icon} />
+          </div>
         </ClickableCard>
       </div>
     </div>
