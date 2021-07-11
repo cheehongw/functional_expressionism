d5ab1931775d2a7aec3e8d06999077b61e453cc9
Chee Hong <c.h.wong2606@gmail.com>
2021-07-11 21:23:19 +0800

    Add loading screen for Location page.
    Close #8

--- src/views/LocationList/LocationList.js
+++ src/views/LocationList/LocationList.js
@@ -1,30 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../components/Header.js";
-import { List } from "@material-ui/core";
+import { CircularProgress, List, Typography } from "@material-ui/core";
 import LocationItem from "./LocationItem.js";
 import { CollapsingChipMenu } from "../../components/CollapsingChipMenu";
 import sortbyDistance from "../../utils/sortByDistance.js";
+import styles from './LocationList.module.css'
 
 const geo = navigator.geolocation;
 
 const LocationList = () => {
-  const [locationList, setLocationList] = useState([]);
+  const [locationList, setLocationList] = useState();
   //used to determine the display state of toolTip
   const [sortingBy, setSortingBy] = useState("Alphabetical");
   const [currPos, setCurrPos] = useState();
 
   useEffect(() => {
     fetch('https://functional-expressionism-api.herokuapp.com/locations')
-    //fetch('http://localhost:3000/locations')
+      //fetch('http://localhost:3000/locations')
       .then(result => result.json())
       .then(
         result => {
           setLocationList(result);
         })
-    },[]);
+  }, []);
 
   useEffect(() => {
-    if (sortingBy === "Distance") {
+    if (locationList === undefined) {
+      
+    } else if (sortingBy === "Distance") {
       geo.getCurrentPosition((pos) => {
         setLocationList((locationList) =>
           sortbyDistance(locationList, {
@@ -63,15 +66,20 @@ const LocationList = () => {
       />
 
       <List disablePadding={true} >
-        {locationList.map(
-          (l) => (
-            <LocationItem
-              key={l.locationName}
-              currPos={currPos}
-              toolTip={sortingBy}
-              locationObj = {l}
-            />
-          ))}
+        {locationList === undefined
+          ? <div className={styles.loading}>
+            <CircularProgress size={'4rem'}></CircularProgress>
+            <Typography variant={"h4"}> Fetching data... </Typography>
+          </div>
+          : locationList.map(
+            (l) => (
+              <LocationItem
+                key={l.locationName}
+                currPos={currPos}
+                toolTip={sortingBy}
+                locationObj={l}
+              />
+            ))}
       </List>
     </div>
   );
